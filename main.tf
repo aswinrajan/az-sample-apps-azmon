01aa078a90a4513fa631bfa1ccc0ca50a972f670
Aswin Soundararajan <aswin.soundararajan@gmail.com>
2022-06-20 22:44:02 +0000

    fixde formatting

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
- terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "aswinrajan"
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "aswinrajan"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "az-sample-apps-azmon"
-         }
-       }
-     }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "az-sample-apps-azmon"
+    }
+  }
+}
 
 module "cluster" {
   source = "./modules/appservice/"
