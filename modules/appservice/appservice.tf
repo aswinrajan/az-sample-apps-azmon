14c554ea5028df96ffd290bc85d8c4b4040ad980
Aswin Soundararajan <aswin.soundararajan@gmail.com>
2022-06-24 06:19:40 +0000

    fixed cource control block

--- modules/appservice/appservice.tf
+++ modules/appservice/appservice.tf
@@ -16,11 +16,11 @@ resource "azurerm_windows_web_app" "dotnet-webapp" {
   resource_group_name = azurerm_resource_group.dotnetrg.name
   location            = azurerm_service_plan.dotnet-serviceplan.location
   service_plan_id     = azurerm_service_plan.dotnet-serviceplan.id
-source_control {
-    repo_url           = "https://github.com/Azure-Samples/app-service-web-dotnet-get-started"
-    branch             = "master"
-    manual_integration = true
-    use_mercurial      = false
-  }
   site_config {}
+}
+
+resource "azurerm_app_service_source_control" "dotnet-webapp-sourcecontrol" {
+  app_id   = azurerm_windows_web_app.dotnet-webapp.id
+  repo_url = "https://github.com/Azure-Samples/app-service-web-dotnet-get-started"
+  branch   = "master"
 }
