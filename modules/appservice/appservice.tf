6ed54092db88f8e17fe3e2c61b7f7cbf87c9dcc7
Aswin Soundararajan <aswin.soundararajan@gmail.com>
2022-06-24 05:58:48 +0000

    added appserviceplan & appservice

--- modules/appservice/appservice.tf
+++ modules/appservice/appservice.tf
@@ -2,3 +2,19 @@ resource "azurerm_resource_group" "mainrg" {
   name     = "${var.prefix}-resources"
   location = var.location
 }
+
+resource "azurerm_service_plan" "main-serviceplan" {
+  name                = "${var.prefix}-appsp"
+  resource_group_name = azurerm_resource_group.mainrg.name
+  location            = azurerm_resource_group.main.location
+  sku_name            = "B1"
+}
+
+resource "azurerm_windows_web_app" "main-webapp" {
+  name                = "${var.prefix}-webapp"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_service_plan.main-serviceplan.location
+  service_plan_id     = azurerm_service_plan.main-serviceplan.id
+
+  site_config {}
+}
