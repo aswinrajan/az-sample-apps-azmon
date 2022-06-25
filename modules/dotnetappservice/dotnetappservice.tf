ee01382e05934ebc508f617decb889524f2d0fff
Aswin Soundararajan <aswin.soundararajan@gmail.com>
2022-06-25 10:30:17 +0000

    added appinsights & LAW

--- modules/dotnetappservice/dotnetappservice.tf
+++ modules/dotnetappservice/dotnetappservice.tf
@@ -8,6 +8,22 @@ resource "random_integer" "random" {
   max = 50000 
 }
 
+resource "azurerm_log_analytics_workspace" "appsvc-az-LAW" {
+  name                = "appsvc-LAW"
+  location            = azurerm_resource_group.dotnetrg.location
+  resource_group_name = azurerm_resource_group.dotnetrg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "dotnet-ai" {
+  name                = "${var.prefix}-ai"
+  location            = azurerm_resource_group.dotnetrg.location
+  resource_group_name = azurerm_resource_group.dotnetrg.name
+  workspace_id        = azurerm_log_analytics_workspace.appsvc-az-LAW.id
+  application_type    = "web"
+}
+
 resource "azurerm_service_plan" "dotnet-serviceplan" {
   name                = "${var.prefix}-appsp"
   resource_group_name = azurerm_resource_group.dotnetrg.name
@@ -21,11 +37,11 @@ resource "azurerm_windows_web_app" "dotnet-webapp" {
   resource_group_name = azurerm_resource_group.dotnetrg.name
   location            = azurerm_service_plan.dotnet-serviceplan.location
   service_plan_id     = azurerm_service_plan.dotnet-serviceplan.id
-  site_config {}
+  site_config {
+    application_insights_key = azurerm_application_insights.dotnet-ai.instrumentation_key
+  }
 }
 
-resource "azurerm_app_service_source_control" "dotnet-webapp-sourcecontrol" {
-  app_id   = azurerm_windows_web_app.dotnet-webapp.id
-  repo_url = "https://github.com/Azure-Samples/app-service-web-dotnet-get-started"
-  branch   = "master"
-}
+
+
+
